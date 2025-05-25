8cc984e5c23bf700862620286077b140a7355944
Jaime <jaimeweingneto@gmail.com>
2025-05-24 21:33:58 -0300

    Forgotten last commit file.

--- server/src/trpc/types.ts
+++ server/src/trpc/types.ts
@@ -3,8 +3,15 @@ import { createContext } from "@/trpc/context";
 export type Context = Awaited<ReturnType<typeof createContext>>
 
 // Requisição genérica de query/mutation protegida
-export type TRPCRequest<TInput> = {
+export type TRPCProtectedRequest<TInput> = {
   ctx: Context & { user: string; role: string };
+  input: TInput;
+  type?: "query" | "mutation" | "subscription";
+  path?: string;
+};
+
+export type TRPCPublicRequest<TInput> = {
+  ctx: Context & { user?: string; role?: string };
   input: TInput;
   type?: "query" | "mutation" | "subscription";
   path?: string;
